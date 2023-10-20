d29ac0a3410cc679a1682e52b51a70439af188e9
matuyama <143941798+matuyama@users.noreply.github.com>
2023-10-20 08:45:24 +0000

    [add]商品登録一時保管

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,21 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    
+  end
+
   def index
     @items = Item.all
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:explanation,:excluding_tax_price,:is_on_sale)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
+  belongs_to :gen
   enum is_on_sale:{ on_sale:true, sales_stop:false }
 end
