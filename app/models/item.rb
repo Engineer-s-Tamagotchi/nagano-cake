7b46ddef9f0a069ea1e1d9a2123c0d8181e96601
matuyama <143941798+matuyama@users.noreply.github.com>
2023-10-21 05:16:41 +0000

    [add]商品新規登録

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
-  belongs_to :gen
+  belongs_to :genre
+  has_one_attached :item_image
   enum is_on_sale:{ on_sale:true, sales_stop:false }
+  
+  def get_item_image(width,height)
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      item_image.attach(io: File.open(file_path),filename: 'default-image.jpg',content_type: 'image/jpeg')
+    end
+    
+    profile_image.cariant(resize_to_limit: [width,heigth]).processed
+    
+  end
+  
 end
