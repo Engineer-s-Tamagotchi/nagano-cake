8dd3caaae06f6d73e6876f50eba7beac56de0323
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-21 05:23:27 +0000

    [Add]モデルにアソシエーションとバリデーションを追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,20 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :addresses, dependent: destory
+  has_many :orders, dependent: destroy
+  has_many :cart_items, dependent: destroy
+
+  with_options presence: do
+    validates :last_name
+    validates :first_name
+    validates :last_name_furigana
+    validates :first_name_furigana
+    validates :postal_code
+    validates :address
+    validates :telephone_number
+    validates :email
+  end
+
 end
