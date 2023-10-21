f6708b66c89970df1d1eb0f0c8a7c109cfe4f088
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-21 05:40:35 +0000

    [Fix]full_nameメソッドとfull_name_furiganaメソッドにスペース追加修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -15,8 +15,8 @@ class Customer < ApplicationRecord
     validates :email
   end
 
-  def full_name = self.last_name + "" + self.first_name
+  def full_name = self.last_name + "　" + self.first_name
 
-  def full_name_furigana = self.last_name_furigana + "" + self.first_name_furigana
+  def full_name_furigana = self.last_name_furigana + "　" + self.first_name_furigana
 
 end
