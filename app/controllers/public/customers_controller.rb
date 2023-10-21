a5facdc7182b28a5e492655d9c49edeb5f2b02bd
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-21 04:29:32 +0000

    [Add]コントローラにアクション追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,15 +1,20 @@
 class Public::CustomersController < ApplicationController
   
   def show
-    
+    @customer = Customer.find(current_customer)
   end
   
   def edit
-    
+    @customer = Customer.find(current_customer)
   end
   
   def update
-    
+    @customer = Customer.find(current_customer)
+    if @customer.update(customer_params)
+      redirect_to mypage_customers_path(current_customer)
+    else
+      render :edit
+    end
   end
   
   def unsubscribe
