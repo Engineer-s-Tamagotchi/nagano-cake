8115ba14241834034bdd0080e65e3e5fbaadeba0
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-21 05:33:47 +0000

    [Add] customers/show.html.erb作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,9 +4,9 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :addresses, dependent: destory
-  has_many :orders, dependent: destroy
-  has_many :cart_items, dependent: destroy
+  has_many :addresses, dependent: :destory
+  has_many :orders, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
 
   with_options presence: do
     validates :last_name
