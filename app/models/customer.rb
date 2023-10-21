be3d9804e822e4d96886e40e83505fdf1d6a7f4a
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-21 05:37:15 +0000

    [Remove] モデルからアソシエーションを削除

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,10 +4,6 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :addresses, dependent: :destory
-  has_many :orders, dependent: :destroy
-  has_many :cart_items, dependent: :destroy
-
   with_options presence: do
     validates :last_name
     validates :first_name
