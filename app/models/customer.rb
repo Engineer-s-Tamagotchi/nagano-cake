6ff7dfd1a5462ae5763cbe23d349e82101ad5b0f
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-22 00:49:30 +0000

    [Add]customerモデルにアソシエーションを追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,10 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :addresses, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
   with_options presence: do
     validates :last_name
     validates :first_name
