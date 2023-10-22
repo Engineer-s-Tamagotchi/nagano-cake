a8531eecbf1550c6e4cbdfd35949abd3eef6c73f
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-22 03:39:47 +0000

    [Add]ログアウト後の遷移先を指定

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -7,6 +7,10 @@ class Public::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     root_path
   end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
   # GET /resource/sign_in
   # def new
