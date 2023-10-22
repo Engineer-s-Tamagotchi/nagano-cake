67759262ebfac6ed70ef7c318146401d13687c64
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-22 02:31:15 +0000

    [Fix]customer_stateメソッドの真偽値確認コードを修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -33,7 +33,7 @@ class Public::SessionsController < Devise::SessionsController
   def customer_state
     @customer = Customer.find_by(email: params[:customer][:email])
     return if !@customer
-    if @customer.valid_password?(params[:customer][:password]) && (@customer.is_active == false)
+    if @customer.valid_password?(params[:customer][:password]) && !@customer.is_active
       flash[:notice] = "退会済みです。再度ご登録ください。"
       redirect_to new_customer_registration_path
     end
