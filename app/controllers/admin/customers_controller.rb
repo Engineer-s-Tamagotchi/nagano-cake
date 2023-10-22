c7e2cae3ad34bac0c1c109fda643ac60024f9323
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-22 05:06:48 +0000

    [Add]admin/customersコントローラ内にアクションを追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,19 +1,31 @@
 class Admin::CustomersController < ApplicationController
 
   def index
-
+    @customers = Customer.page(params[:page]).per(2)
   end
 
   def show
-
+    @customer = Customer.find(params[:id])
   end
 
   def edit
-
+    @customer = Customer.find(params[:id])
   end
 
   def update
-
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:success] = "登録情報の変更が完了しました."
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_furigana, :first_name_furigana, :postal_code, :address, :telephone_number, :email, :is_active)
   end
 
 end
