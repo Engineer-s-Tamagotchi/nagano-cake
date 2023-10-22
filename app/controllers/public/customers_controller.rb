27377ddb96ff4bff9ae51967231d5c686800e485
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-22 04:23:40 +0000

    [Fix]quitアクションのcustomer.findをCustomer.findに修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,13 +1,13 @@
 class Public::CustomersController < ApplicationController
-  
+
   def show
     @customer = Customer.find(current_customer.id)
   end
-  
+
   def edit
     @customer = Customer.find(current_customer.id)
   end
-  
+
   def update
     @customer = Customer.find(current_customer.id)
     if @customer.update(customer_params)
@@ -17,21 +17,21 @@ class Public::CustomersController < ApplicationController
       render :edit
     end
   end
-  
+
   def unsubscribe
   end
-  
+
   def quit
-    @customer = customer.find(current_customer.id)
+    @customer = Customer.find(current_customer.id)
     @customer.update(is_active: false)
     reset_session
     redirect_to root_path
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_furigana, :first_name_furigana, :postal_code, :address, :telephone_number, :email)
   end
-  
+
 end
