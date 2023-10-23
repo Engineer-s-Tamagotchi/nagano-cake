5d7c7d847471fa1c94a2a7e321560ba7a8692032
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-23 09:35:41 +0000

    [Add]public/orders_controllerのconfirmアクションの作成(条件分岐中)

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,7 +5,17 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    
+    @cart_items = Cart_items.find_by(customer_id: current_customer.id)
+    @payment_method = params[:order][:payment_method]
+    if params[:selected_address] == 0
+      @postal_code = current_customer.postal_code
+      @address = current_customer.address
+      @name = current_customer.last_name + current_customer.first_name
+    elsif params[:selected_address] == 1
+      address_id = params[:order][:address_id]
+      @address = Address.find_by(id: address_id)
+      @postal_code = @address.postal_code
+    end
   end
 
   def complete
