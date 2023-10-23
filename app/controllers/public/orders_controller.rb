6e6a94672e4d0beca145f2d2c0410a8e6f21ae12
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-23 07:55:12 +0000

    [Add]public/ordersのnewアクションを追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,27 +1,27 @@
 class Public::OrdersController < ApplicationController
-  
+
   def new
-    
+    @order = Order.new
   end
-  
+
   def confirm
-    
+
   end
-  
+
   def complete
-    
+
   end
-  
+
   def create
-    
+
   end
-  
+
   def index
-    
+
   end
-  
+
   def show
-    
+
   end
-  
+
 end
