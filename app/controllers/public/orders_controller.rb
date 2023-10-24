e53cdc38ea623823e12a1a36cc9166721a2214a3
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-24 01:54:50 +0000

    [Add]confirmアクションの作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,15 +7,24 @@ class Public::OrdersController < ApplicationController
   def confirm
     @order = Order.new
     @cart_items = CartItem.where(customer_id: current_customer.id)
-    @payment_method = params[:order][:payment_method]
+    @order_payment_method = params[:order][:payment_method]
     if params[:selected_address] == 0
-      @postal_code = current_customer.postal_code
-      @address = current_customer.address
-      @name = current_customer.last_name + current_customer.first_name
+      @order_postal_code = current_customer.postal_code
+      @order_address = current_customer.address
+      @order_name = current_customer.last_name + current_customer.first_name
     elsif params[:selected_address] == 1
-      address_id = params[:order][:address_id]
-      @address = Address.find_by(id: address_id)
-      @postal_code = @address.postal_code
+      if Address.exists?(id: params[:order][:address_id])
+        address = params[:order][:address_id]
+        @order_postal_code = address.postal_code
+        @order_address = address.address
+        @order_name = address.name
+      else
+        render :new
+      end
+    else
+      @order_postal_code = params[:order][:postal_code]
+      @order_address = params[:order][:address]
+      @order_name = params[:order][:name]
     end
   end
 
