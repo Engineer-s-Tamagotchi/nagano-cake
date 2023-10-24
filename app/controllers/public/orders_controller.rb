773be6fe1019fc6dd43c7c16123889e1bfdfcc67
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-24 07:39:32 +0000

    [Add]confirmアクション&confirm.html.erb作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,12 +7,15 @@ class Public::OrdersController < ApplicationController
   def confirm
     @order = Order.new
     @cart_items = CartItem.where(customer_id: current_customer.id)
-    @order_payment_method = params[:order][:payment_method]
-    if params[:selected_address] == 0
+    @order_payment_method = params[:order][:payment_method].to_i
+    @order_postage = 800
+    @total_price = 0
+    selected_address = params[:order][:selected_address].to_i
+    if selected_address == 0
       @order_postal_code = current_customer.postal_code
       @order_address = current_customer.address
       @order_name = current_customer.last_name + current_customer.first_name
-    elsif params[:selected_address] == 1
+    elsif selected_address == 1
       if Address.exists?(id: params[:order][:address_id])
         address = params[:order][:address_id]
         @order_postal_code = address.postal_code
@@ -21,19 +24,35 @@ class Public::OrdersController < ApplicationController
       else
         render :new
       end
-    else
+    elsif selected_address == 2
       @order_postal_code = params[:order][:postal_code]
       @order_address = params[:order][:address]
       @order_name = params[:order][:name]
+    else
+      render :new
+    end
+  end
+  
+  def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    if @order.save
+      @cart_items = CartItem.where(customer_id: current_customer.id)
+      @cart_items.each do |cart_item|
+        order_detail = OrderDetail.new
+        order_detail.order_id = @order.id
+        order_detail.item_id = cart_item.item_id
+        order_detail.amount = cart_item.amount
+        order_detail.including_tax_price = cart_item.including_tax_price
+        order_detail.save
+      end
+      redirect_to complete_orders_path
+    else
+      render :new
     end
   end
 
   def complete
-    
-
-  end
-
-  def create
 
   end
 
@@ -48,7 +67,7 @@ class Public::OrdersController < ApplicationController
 end
 
 private
-  
+
   def order_params
     params.require(:order).permit(:name, :billing_price, :payment_method, :postage, :postal_code, :address, :status)
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -13,7 +13,7 @@ class Order < ApplicationRecord
     validates :status
   end
 
-  enum payment_method:{ クレジットカード: 0, 現金振込: 1 }
-  enum status:{ 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4 }
+  enum payment_method:{ credit_card: 0, transfer: 1 }
+  enum status:{ wait_for_payment: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
 
 end
